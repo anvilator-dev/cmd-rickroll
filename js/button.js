b052edd3ab1e175d54c624f734e0d6f05078625c
Anvilator-dev <kinosoftinc@gmail.com>
2021-11-15 13:34:06 +0530

    HTML, CSS and JS
    Checked code.

---
+++ js/button.js
@@ -0,0 +1,3 @@
+document.getElementById("run").onclick = function () {
+    location.href = "https://bit.ly/2PFcOQ1";
+};
---
+++ js/github.js
@@ -0,0 +1,3 @@
+document.getElementById("github").onclick = function () {
+    location.href = "https:/github.com/anvilator-dev/cmd-rickroll";
+};
